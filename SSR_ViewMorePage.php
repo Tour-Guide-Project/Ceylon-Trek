412c4f07c47355a85af5760db30a7d5545cb3e53
sulakshanee <sulakshanee111@gmail.com>
2020-10-04 23:33:25 +0530

    SSR_VMP commit

---
+++ SSR_ViewMorePage.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Smart Search Results Page</title>
+	<link rel="stylesheet" type="text/css" href="css/SSR_ViewMorePage.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body style="background-image: url('img/2.jpg'); background-size:cover;background-position: center center;background-attachment: fixed; background-repeat:no-repeat;">
+
+	<div class="container">
+		<h1 class="ttl"><b>Hikkaduwa</b></h1>
+		<div class="image">
+			<img src="img/1.jpg" alt="Paris" style="width:40%;">
+		</div>
+
+		<div>
+			<p class="paragraph">Hikkaduwa is one of the most beautiful place in Sri Lanka. Hikkaduwa is one of the most beautiful place in Sri Lanka. Hikkaduwa is one of the most beautiful place in Sri Lanka. Hikkaduwa is one of the most beautiful place in Sri Lanka.</p>
+
+			<p class="paragraph">Hikkaduwa is one of the most beautiful place in Sri Lanka. Hikkaduwa is one of the most beautiful place in Sri Lanka. Hikkaduwa is one of the most beautiful place in Sri Lanka. Hikkaduwa is one of the most beautiful place in Sri Lanka.</p>
+
+			<p class="paragraph">Hikkaduwa is one of the most beautiful place in Sri Lanka. Hikkaduwa is one of the most beautiful place in Sri Lanka.</p>
+		</div>
+	</div>
+
+</body>
+</html>
