0689254e8ede00dbc7181a6d7c5dd1281f6ef3e2
sulakshanee <sulakshanee111@gmail.com>
2020-10-04 22:01:18 +0530

    SSRP commit

---
+++ SmartSearchResultsPage.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Smart Search Results Page</title>
+	<link rel="stylesheet" type="text/css" href="css/smartSearchResultsPage.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body style="background-image: url('img/18.jpg'); background-size:cover;background-position: center center;background-attachment: fixed; background-repeat:no-repeat;">
+
+	<div class="container">
+		<div class="section">
+			<div class="image">
+				<img src="img/tree.jpg" alt="Paris" style="width:40%;">
+			</div>
+
+			<div>
+				<p class="paragraph">Hikkaduwa is one of the most beautiful place in Sri Lanka</p>
+			</div>
+
+			<div class="btn">
+				<button class="viewButton">View</button>
+			</div>
+		</div>
+
+		<div class="section">
+			<div class="image">
+				<img src="img/tree.jpg" alt="Paris" style="width:40%;">
+			</div>
+
+			<div>
+				<p class="paragraph">Hikkaduwa is one of the most beautiful place in Sri Lanka</p>
+			</div>
+
+			<div class="btn">
+				<button class="viewButton">View</button>
+			</div>
+		</div>
+
+		<div class="section">
+			<div class="image">
+				<img src="img/tree.jpg" alt="Paris" style="width:40%;">
+			</div>
+
+			<div>
+				<p class="paragraph">Hikkaduwa is one of the most beautiful place in Sri Lanka</p>
+			</div>
+
+			<div class="btn">
+				<button class="viewButton">View</button>
+			</div>
+		</div>
+
+		<div class="section">
+			<div class="image">
+				<img src="img/tree.jpg" alt="Paris" style="width:40%;">
+			</div>
+
+			<div>
+				<p class="paragraph">Hikkaduwa is one of the most beautiful place in Sri Lanka</p>
+			</div>
+
+			<div class="btn">
+				<button class="viewButton">View</button>
+			</div>
+		</div>
+
+		<div class="section">
+			<div class="image">
+				<img src="img/tree.jpg" alt="Paris" style="width:40%;">
+			</div>
+
+			<div>
+				<p class="paragraph">Hikkaduwa is one of the most beautiful place in Sri Lanka</p>
+			</div>
+
+			<div class="btn">
+				<button class="viewButton">View</button>
+			</div>
+		</div>
+
+		<div class="section">
+			<div class="image">
+				<img src="img/tree.jpg" alt="Paris" style="width:40%;">
+			</div>
+
+			<div>
+				<p class="paragraph">Hikkaduwa is one of the most beautiful place in Sri Lanka</p>
+			</div>
+
+			<div class="btn">
+				<button class="viewButton">View</button>
+			</div>
+		</div>
+
+		<div class="section">
+			<div class="image">
+				<img src="img/tree.jpg" alt="Paris" style="width:40%;">
+			</div>
+
+			<div>
+				<p class="paragraph">Hikkaduwa is one of the most beautiful place in Sri Lanka</p>
+			</div>
+
+			<div class="btn">
+				<button class="viewButton">View</button>
+			</div>
+		</div>
+
+		<div class="section">
+			<div class="image">
+				<img src="img/tree.jpg" alt="Paris" style="width:40%;">
+			</div>
+
+			<div>
+				<p class="paragraph">Hikkaduwa is one of the most beautiful place in Sri Lanka</p>
+			</div>
+
+			<div class="btn">
+				<button class="viewButton">View</button>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
