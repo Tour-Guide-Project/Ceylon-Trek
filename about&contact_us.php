9fc987cae03f054131cb67afc01c3a3f0c827e6c
Piyumi047 <piyumidulanjali1998@gmail.com>
2020-10-05 14:51:48 +0530

    about_us & contact us

---
+++ about&contact_us.php
@@ -0,0 +1,84 @@
+<html lang="en">
+    <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>About_us</title>
+        <link rel='stylesheet' type='text/css' media='screen' href='css/contact_us.css'>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+
+    <body>
+        <div class="about_header">
+           <h4>ABOUT US</h4>
+        </div>
+        <div class="content 1">
+            <h3>| WHO WE ARE</h3>
+             <p>Our web site http//ceylontreck.com has been founded local tour guides , to have better visibility and a fair price on their services.</p>
+        </div>
+
+        <div class="content 2">
+            <h3>| EXCELLENT SERVICES AND A REVIEW PLATFORM</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit corporis ipsa consequatur facere sapiente ea harum exercitationem fugiat provident impedit omnis, veniam dolor laborum numquam incidunt possimus sed illum atque. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam obcaecati nisi corrupti consectetur magnam minus quod perferendis, voluptate maiores enim tempora assumenda provident eos itaque iste excepturi earum libero veritatis! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus debitis harum commodi earum voluptas officiis maxime dolores ad iusto explicabo modi nemo est maiores asperiores velit odit, vel consequuntur corporis.</p>
+        </div>
+
+        <div class="content 3">
+            <h3>| OUR GOAL</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dolores aspernatur, tempora eveniet laudantium corrupti, laborum natus quae esse error enim architecto excepturi consectetur voluptatem voluptas voluptatum illum debitis numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus esse itaque officia quisquam repudiandae quo omnis architecto rerum dolorum eligendi quibusdam asperiores, placeat dolores nesciunt porro maxime vero magnam. Vero?</p>
+        </div>
+
+        
+
+        <div class="contactus">
+             <table>
+                 <tr>
+                     <td>
+                         <div class="contact_form" >
+                            <form>
+                                <label for="name">Name</label><br>
+                                <input type="text" id="name" name="name"><br>
+                                <label for="email">Email</label><br>
+                                <input type="email" id="email" name="email"><br>
+                                <label for="subject">Subject</label><br>
+                                <input type="subject" id="subject" name="subject"><br>
+                                <label for="message">Message</label><br>
+                                <textarea name="message" cols="88" rows="5"></textarea><br>
+                                <button name="send">Send</button>
+                            </form>
+
+                            <div style="text-align: center;">
+                                <div
+                                    class="g-recaptcha" 
+                                    data-sitekey="YOUR SITE KEY"    
+                                ></div>
+                            </div>
+                         </div>
+                     </td>
+                     <td>
+                         <div class="info">
+                             <h4>CONTACT INFO</h4>
+                               <div><h5>NO:10 ,Ceylon Treck,</h5></div>
+                               <div><h5>Wijethunga Mawatha,</h5></div>
+                               <div><h5>Colombo 07.</h5></div>
+                               <div><h5>Mobile:+94783456278</h5></div>
+                               <div><h5>Tel:+94412389456</h5></div>
+                               <div><h5>ceylontreck@gmail.com</h5></div>
+
+
+                               <div class="contact-icon">
+                                <i class="fa fa-facebook" aria-hidden="true"></i>
+                                <i class="fa fa-twitter" aria-hidden="true"></i>
+                                <i class="fa fa-instagram" aria-hidden="true"></i>
+                                <i class="fa fa-rss" aria-hidden="true"></i>
+                            </div><!--contact-icon-->
+                         </div>
+                         
+                     </td>
+                        
+
+                     
+                 </tr>
+             </table>
+        </div><!--contactus-->
+
+    </body>
+</html>
