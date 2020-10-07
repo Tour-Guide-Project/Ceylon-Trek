0298c8d0d552cc23798771e6ee7a67302b939bfd
Piyumi047 <piyumidulanjali1998@gmail.com>
2020-10-07 21:43:28 +0530

    Guide dash borard

---
+++ guide dash board.php
@@ -0,0 +1,52 @@
+<html  lang="en">
+    <head>
+        <title>Guide dash board</title>
+        <link rel='stylesheet' type='text/css' media='screen' href='css/guide dash board.css'>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+
+    <body >
+        <div class="wrapper">
+            <div class="main_board">
+                <div class="side_bar">
+                    <img src="img/reviewimg.jpg" class="img" >
+                    <button class="edit"> Edit Profile</button><br>
+                  <div class="buttons">
+                    <br><a href="#"><button class="button"><i class="fa fa-commenting fa-1x" aria-hidden="true"></i>Inbox             </button></a>
+                    <br><a href="#"><button class="button"><i class="fa fa-plus-square fa-1x" aria-hidden="true"></i>Create Tour Package</button></a>
+                    <br><a href="#"><button class="button"><i class="fa fa-eye fa-1x" aria-hidden="true"></i>View Tour Package </button></a>
+                    <br><a href="#"><button class="button"><i class="fa fa-fast-forward fa-1x" aria-hidden="true"></i>Up Coming Tours   </button></a>
+                    <br><a href="#"><button class="button"><i class="fa fa-fast-backward fa-1x" aria-hidden="true"></i>Previous Tours     </button></a>
+                  </div><!--buttons-->        
+                </div><!--side_bar-->
+
+                <div class="schedule">
+                    <h1>Today's Schedule</h1><br>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos cum amet quis voluptatibus, recusandae itaque voluptate architecto excepturi harum quaerat iure quisquam esse consectetur vitae adipisci debitis, earum necessitatibus! Soluta?Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quasi voluptas quam ea. Nesciunt nobis quae tempore incidunt impedit ex fuga repudiandae praesentium est, corporis assumenda obcaecati atque qui expedita!Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur eveniet nemo impedit voluptatum cumque vero fugiat unde sint earum, harum ullam velit voluptatibus ducimus quae tenetur. Officia ducimus praesentium non!<br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos cum amet quis voluptatibus, recusandae itaque voluptate architecto excepturi harum quaerat iure quisquam esse consectetur vitae adipisci debitis, earum necessitatibus! Soluta?Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quasi voluptas quam ea. Nesciunt nobis quae tempore incidunt impedit ex fuga repudiandae praesentium est, corporis assumenda obcaecati atque qui expedita!Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur eveniet nemo impedit voluptatum cumque vero fugiat unde sint earum, harum ullam velit voluptatibus ducimus quae tenetur. Officia ducimus praesentium non!<br><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos cum amet quis voluptatibus, recusandae itaque voluptate architecto excepturi harum quaerat iure quisquam esse consectetur vitae adipisci debitis, earum necessitatibus! Soluta?Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quasi voluptas quam ea. Nesciunt nobis quae tempore incidunt impedit ex fuga repudiandae praesentium est, corporis assumenda obcaecati atque qui expedita!Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur eveniet nemo impedit voluptatum cumque vero fugiat unde sint earum, harum ullam velit voluptatibus ducimus quae tenetur. Officia ducimus praesentium non!</p>   
+                    
+                        <div><button class="schedule_button">View Tommorow</button></div>
+                        <button class="schedule_button ">Schedule Complete</button>
+                        <button class="schedule_button ">Schedule Not Complete</button>
+                 
+                </div><!--schedule-->
+
+                <div class="guidecalender">
+                     
+                </div><!--guidecalender-->
+
+                <div class="corner_buttons">
+                        <div>
+                            <button class="cobutton"><i class="fa fa-credit-card" aria-hidden="true"></i>Pay Sysytem Fee</button>
+                        </div>
+
+                        <div>
+                            
+                            <button class="cobutton"><i class="fa fa-phone" aria-hidden="true"></i>Contact Ceylon Treck</button>
+                        </div>
+                </div><!--corner_button-->
+            </div><!--main_board-->       
+        </div><!--wrapper-->
+
+
+    </body>
+</html>
