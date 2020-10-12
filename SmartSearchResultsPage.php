80e232ef9e410a8a68a71f23006ed9cf112b7e72
sulakshanee <sulakshanee111@gmail.com>
2020-10-12 19:24:09 +0530

    I Changed SSRP

--- SmartSearchResultsPage.php
+++ SmartSearchResultsPage.php
@@ -3,12 +3,17 @@
 <head>
 	<title>Smart Search Results Page</title>
 	<link rel="stylesheet" type="text/css" href="css/smartSearchResultsPage.css">
+	<link rel="stylesheet" href="css/top_bar.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body style="background-image: url('img/18.jpg'); background-size:cover;background-position: center center;background-attachment: fixed; background-repeat:no-repeat;">
 
-	<div class="container">
+	<?php include('inc/top_bar.php');?>
+
+	<div class="con">
 		<div class="section">
 			<div class="image">
 				<img src="img/tree.jpg" alt="Paris" style="width:40%;">
@@ -121,6 +126,8 @@
 			</div>
 		</div>
 	</div>
+
+<?php include('inc/footer.php');?>p
 
 </body>
 </html>
