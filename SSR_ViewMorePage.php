29341043dcfd56a7d1528ee1a581c313a1559be1
sulakshanee <sulakshanee111@gmail.com>
2020-10-12 19:26:47 +0530

    I Changed SSR_VMP

--- SSR_ViewMorePage.php
+++ SSR_ViewMorePage.php
@@ -3,12 +3,17 @@
 <head>
 	<title>Smart Search Results Page</title>
 	<link rel="stylesheet" type="text/css" href="css/SSR_ViewMorePage.css">
+	<link rel="stylesheet" href="css/top_bar.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body style="background-image: url('img/2.jpg'); background-size:cover;background-position: center center;background-attachment: fixed; background-repeat:no-repeat;">
 
-	<div class="container">
+	<?php include('inc/top_bar.php');?>
+
+	<div class="con">
 		<h1 class="ttl"><b>Hikkaduwa</b></h1>
 		<div class="image">
 			<img src="img/1.jpg" alt="Paris" style="width:40%;">
@@ -22,6 +27,8 @@
 			<p class="paragraph">Hikkaduwa is one of the most beautiful place in Sri Lanka. Hikkaduwa is one of the most beautiful place in Sri Lanka.</p>
 		</div>
 	</div>
+
+<?php include('inc/footer.php');?>
 
 </body>
 </html>
